5d91e580555308f155fbfead050e6650e69c4291
Problem <pboyd04@gmail.com>
2014-09-15 18:29:45 +0000

    Initial commit of index page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once('class.SecurePage.php');
+$page = new SecurePage('Burning Flipside - Secure Sites');
+
+$page->body .= '
+<div id="content">
+    <h1>Welcome to the Burning Flipside Secure System</h1>
+    <p>This system allows you to register for tickets, transfer tickets, register your theme camp or art project, etc..</p>
+    <h1>What would you like to do?</h1>
+    <ul>
+        <li><a href="/tickets/index.php">Ticket Registration</a></li>
+        <li><a href="/tickets/transfer.php">Ticket Transfer</a></li>
+        <li><a href="/theme_camp/registration.php">Theme Camp Registration</a></li>
+        <li><a href="/art/registration.php">Art Project Registration</a></li>
+        <li><a href="/event/index.php">Event Registration</a></li>
+    </ul>
+</div>';
+
+$page->print_page();
+?>
