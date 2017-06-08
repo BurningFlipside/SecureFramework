b57ee6f92258574f262dffa1c4f80d75d1e7004f
Patrick Boyd <pboyd04@gmail.com>
2017-06-08 09:47:35 -0500

    Fix copy paste includes

--- api/v1/index.php
+++ api/v1/index.php
@@ -3,13 +3,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require('Autoload.php');
-require('class.AreasAPI.php');
-require('class.GroupsAPI.php');
-require('class.LeadsAPI.php');
-require('class.PendingUserAPI.php');
-require('class.SessionsAPI.php');
-require('class.UsersAPI.php');
-require('class.ProfilesAPI.php');
+require('class.SecureAPI.php');
 
 $site = new \Http\WebSite();
 $site->registerAPI('/', new SecureAPI());
